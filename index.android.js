f1ddeb50a33513b322498e94db7d8ae0ba61e902
senkovicova1 <36665069+senkovicova1@users.noreply.github.com>
2019-03-12 15:33:43 +0100

    commit

--- index.android.js
+++ index.android.js
@@ -7,6 +7,8 @@ import * as firebase from 'firebase';
 import Rebase from 're-base';
 import 'firebase/firestore';
 
+import com.facebook.FacebookSdk;
+
 const app = firebase.initializeApp(config);
 export let rebase = Rebase.createClass(app.database());
 
